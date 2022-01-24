b91b8f387c54ca4980b26391c08ffaeae0fa6291
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2022-01-24 20:22:55 +0100

    Added function to format texts

--- app.js
+++ app.js
@@ -1,42 +1,25 @@
 const projects = [
   {
-    name: "element 1",
-    github: "link",
-  },
-  {
-    name: "element 1",
-    github: "link",
-  },
-  {
-    name: "element 1",
-    github: "link",
-  },
-  {
-    name: "element 1",
-    github: "link",
-  },
-  {
-    name: "element 1",
-    github: "link",
-  },
-  {
-    name: "element 1",
-    github: "link",
-  },
-  {
-    name: "element 1",
+    name: "QR-code-component",
     github: "link",
   },
 ];
 
 const list = document.getElementById("list");
 
+const formatName = (name) => {
+  return name
+    .split("-")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+};
+
 projects.forEach(({ name, github }, i) => {
   const listItem = document.createElement("li");
   listItem.innerHTML = `
     <a href="/${name}/index.html">
       <img src="/${name}/design/desktop-design.jpg" alt="${name}" />
-      <p>${i + 1}. ${name}</p>
+      <p>${i + 1}. ${formatName(name)}</p>
     </a>
     <div class="links-container">
       <a href="/${name}/index.html" class="view">
