3eee1aa97b2963b4f23c45ec7b5ab5a0d0e44bdc
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2022-01-27 23:59:24 +0100

    Finished day 4 project

---
+++ Ambience/README.md
@@ -0,0 +1,47 @@
+# Ambience - relax page
+
+This is a project for the 4nd day of 100 days challenge. I wanted to make something pretty but simple.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+- [Attribution] (#Attribution)
+
+## Overview
+
+### Day 2 of 100 days challenge
+
+I might have a problem with making decisions. In the beginning I wanted to make a coffee shop ambience. I gave up after 1 hour of trying to find the perfect background. Then I thought about a medieval tavern ambience - Skyrin/Witcher type - unfortunately, I couldn't find any good background. I realized that I should try to learn how to make a pixel art - I should add it to my "to-do list". Finally I found this parallax setup. I wanted to do something with paralax for quite some time so I found some youtube videos and started coding.
+
+### Screenshot
+
+![](./design/desktop-design.jpg)
+
+### Links
+
+- Solution URL: [here](https://github.com/joaskr/100-days-challenge/tree/main/Ambience)
+- Live Site URL: [here](https://100-days-challenge-azure.vercel.app/Ambience/index.html)
+
+## My process
+
+### Built with
+
+- Parallax.js
+
+## Author
+
+- Website - [Add your name here](https://www.your-site.com)
+- Frontend Mentor - [@joaskr](https://www.frontendmentor.io/profile/joaskr)
+
+## Attribution
+
+- Background - [The Island: Parallax Ready 2D Background for Platformer or Side-Scroller by saukgp](https://saurabhkgp.itch.io/the-island-parallax-background-platformer-side-scroller)
+- Music - [Free Ambient Music by Mayra](https://mayragandra.itch.io/freeambientmusic)
+- Water sounds - [Water lapping on lake Pukaki,South Island,New Zealand by laughatlantic](https://freesound.org/people/laughatlantic/sounds/528067/)
+- Birds sounds - [Birds In Spring (Scotland) by BurghRecords](https://freesound.org/people/BurghRecords/sounds/463903/)
+- Youtube tutorial for parallax.js - [Awesome Background Parallax Effect on Mousemove by OnlineTutorials](https://www.youtube.com/watch?v=6xVMUo0_hu4)
---
+++ Ambience/app.js
@@ -0,0 +1,66 @@
+const clockNumbers = document.getElementById("clock");
+const greetingText = document.getElementById("greetings");
+const greetingIcon = document.querySelector(".greeting-icon");
+const musicBtn = document.querySelector(".music-chbx");
+const music = document.querySelector(".music");
+const waterBtn = document.querySelector(".water-chbx");
+const water = document.querySelector(".water");
+const birdsBtn = document.querySelector(".birds-chbx");
+const birds = document.querySelector(".birds");
+
+var scene = document.getElementById("scene");
+var parallaxInstance = new Parallax(scene, {
+  relativeInput: true,
+  scalarY: 0, //blocks up and down movement
+});
+
+const getTime = () => {
+  setInterval(() => {
+    //getting time
+    const currentTime = new Date();
+    let currentHour = currentTime.getHours();
+    let currentMinute = currentTime.getMinutes();
+    let currentSecond = currentTime.getSeconds();
+    currentMinute = (currentMinute < 10 ? "0" : "") + currentMinute;
+    currentSecond = (currentSecond < 10 ? "0" : "") + currentSecond;
+    const currentTimeText = `${currentHour}:${currentMinute}:${currentSecond}`;
+    clockNumbers.innerHTML = currentTimeText;
+    //check if you should say morning/afternoon/evening
+    if (currentHour === 24) {
+      greeting = "good morning";
+      greetingText.innerHTML = greeting;
+      greetingIcon.classList.add("fa-coffee");
+    } else if (currentHour >= 1 && currentHour < 13) {
+      greeting = "good morning";
+      greetingText.innerHTML = greeting;
+      greetingIcon.classList.add("fa-coffee");
+    } else if (currentHour >= 13 && currentHour < 17) {
+      greeting = "good afternoon";
+      greetingText.innerHTML = greeting;
+      greetingIcon.classList.add("fa-sun");
+    } else {
+      greeting = "good evening";
+      greetingText.innerHTML = greeting;
+      greetingIcon.classList.add("fa-moon");
+    }
+  }, 1000);
+};
+
+musicBtn.addEventListener("change", function () {
+  music.paused ? music.play() : music_stop(music);
+  music.volume = 0.1;
+});
+waterBtn.addEventListener("change", function () {
+  water.paused ? water.play() : music_stop(water);
+  water.volume = 0.1;
+});
+birdsBtn.addEventListener("change", function () {
+  birds.paused ? birds.play() : music_stop(birds);
+  birds.volume = 0.1;
+});
+function music_stop(audio) {
+  audio.pause();
+  audio.currentTime = 0;
+}
+
+document.addEventListener('"DOMContentLoaded"', getTime());
