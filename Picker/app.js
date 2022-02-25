ff7808f7512ea11125e116b6126cd45f056476a3
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2022-02-25 23:49:48 +0100

    Initial version of day11

---
+++ Picker/app.js
@@ -0,0 +1,42 @@
+const body = document.querySelector(".body");
+const picker = document.getElementById("color-picker");
+const header = document.getElementById("color-header");
+
+const fetchPalette = async (red, green, blue) => {
+  var url = "http://colormind.io/api/";
+  var data = {
+    model: "default",
+    input: [[red, green, blue], "N", "N", "N", "N"],
+  };
+  var http = new XMLHttpRequest();
+
+  http.onreadystatechange = await function () {
+    if (http.readyState == 4 && http.status == 200) {
+      var palette = JSON.parse(http.responseText).result;
+      palette.forEach((color, id) => {
+        let fetchedColor = `${color[0]}, ${color[1]}, ${color[2]}`;
+        let fetchedColorRGB = "rgb(" + fetchedColor + ")";
+        const elemName = "colorDiv" + id;
+        document.getElementById(elemName).style.backgroundColor =
+          fetchedColorRGB;
+        document.getElementById(elemName).innerHTML = fetchedColorRGB;
+      });
+    }
+  };
+
+  http.open("POST", url, true);
+  http.send(JSON.stringify(data));
+};
+
+const setPickerValue = () => {
+  const pickerValue = picker.value;
+  let red = parseInt(pickerValue[1] + pickerValue[2], 16);
+  let green = parseInt(pickerValue[3] + pickerValue[4], 16);
+  let blue = parseInt(pickerValue[5] + pickerValue[6], 16);
+  let rgbColor = `${red}, ${green}, ${blue}`;
+  header.innerHTML = "rgb(" + rgbColor + ")";
+  fetchPalette(red, green, blue);
+};
+
+picker.addEventListener("change", setPickerValue);
+document.addEventListener("DOMContentLoaded", setPickerValue);
